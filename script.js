99cd04ce23fe7b74638d6ed4fbebe08a284bfbe2
Sajid Hussain <sajid6207116@gmail.com>
2024-02-03 10:48:52 +0500

    time

---
+++ script.js
@@ -0,0 +1,18 @@
+// const clock = document.querySelector('#clock')
+
+// setInterval(function(){
+//     let date = new Date()
+//     clock.innerHTML = date.toLocaleTimeString()
+// }, 1000)
+
+
+
+
+
+
+const clock = document.getElementById('clock')
+
+setInterval(function(){
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString()
+}, 1000)
